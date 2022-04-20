9af0d20ac17cf0784241c56e4d566f8c2d235a1b
evanjscallan <evanjscallan@gmail.com>
2022-04-20 15:46:17 -0500

    typofix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,7 +20,7 @@ export default class Layout extends React.Component{
 
 	return(
 		<div className='layout flex-row-ctr'>
-		<p>Fairly Boring)</p><h1>Image Editor</h1>
+		<p>(Fairly Boring)</p><h1>Image Editor</h1>
 			<Filter
 			satParam='saturate'
 			contrastParam='contrast'
